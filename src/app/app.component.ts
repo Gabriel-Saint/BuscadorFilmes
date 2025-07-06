9131c1f54e6e6c002e4c579df1865afcdca4bd29
Gabriel Saint <gabrielsantosayres@gmail.com>
2025-07-06 15:44:18 -0300

    Deploy aplicação buscador de filmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MovieMainPageComponent } from "./features/movie-main-page/movie-main-page/movie-main-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MovieMainPageComponent],
+  imports: [MovieMainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
